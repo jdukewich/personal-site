9a905295f7eca516d0ecb42c43ae7f263d9aa995
John Dukewich <jdukewich@gmail.com>
2021-06-20 12:59:24 -0400

    Basic site structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ResumeComponent,
+    WorkComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
